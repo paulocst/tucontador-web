6573b6a0f31489772191ea50aef19a54bfa7586e
Paulo C Saldarriaga <paulocst@gmail.com>
2019-01-18 11:53:25 -0500

    Component accountant created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountantComponent } from './components/accountant/accountant.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'accountant', component:AccountantComponent}	
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
